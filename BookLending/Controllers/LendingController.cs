ad8713720987e269ecc933df229ef35933732948
Victor Pereira <vic.torpereira@hotmail.com>
2023-09-15 01:40:52 +0100

    2 - Finalizando CRUD

--- BookLending/Controllers/LendingController.cs
+++ BookLending/Controllers/LendingController.cs
@@ -14,11 +14,99 @@ namespace BookLending.Controllers
             _db = db;
         }
 
-
+        // Metodo GET - Pegando informacoes da base de dados
         public IActionResult Index()
         {
             IEnumerable<LendingModel> lendings = _db.Lending;
             return View(lendings);
+        }
+
+        // Metodo GET - Pegando simplesmente a pagina Register no click e mostra a tela
+        [HttpGet]
+        public IActionResult Register() 
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id) 
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            LendingModel lending = _db.Lending.FirstOrDefault(x => x.Id == id);
+
+            if(lending == null) 
+            {
+                return NotFound();
+            }
+
+            return View(lending);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            LendingModel lending = _db.Lending.FirstOrDefault(x => x.Id == id);
+
+            if (lending == null)
+            {
+                return NotFound();
+            }
+
+            return View(lending);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Register(LendingModel lending) 
+        { 
+            if (ModelState.IsValid)
+            { 
+                _db.Lending.Add(lending);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(LendingModel lending)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Lending.Update(lending);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+
+            return View(lending); 
+        }
+
+        [HttpPost]
+        public IActionResult Delete(LendingModel lending)
+        { 
+            if (lending == null)
+            {
+                return NotFound();
+            }
+
+            _db.Lending.Remove(lending);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
--- BookLending/Models/LendingModel.cs
+++ BookLending/Models/LendingModel.cs
@@ -1,10 +1,18 @@
-﻿namespace BookLending.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookLending.Models
 {
     public class LendingModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter the name of the Receiver!!!")]
         public string Receiver { get; set; }
+
+        [Required(ErrorMessage = "Enter the name of the Supplier!!!")]
         public string Supplier { get; set; }
+
+        [Required(ErrorMessage = "Enter the name of the Book!!!")]
         public string BookLend { get; set; }
 
         //Propiedade do banco de dados sempre ira pegar a data atual
